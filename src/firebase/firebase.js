577b4c1fd65e9be24f225bf973a0553d3eed05bb
Terézia Boďová <bodova.terezia13@gmail.com>
2024-11-26 17:35:28 +0100

    4th - firebase updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCe_GxNOKPP4dUp6sSa1Yy-fyFCjryj7KM",
-  authDomain: "user-administration-dff86.firebaseapp.com",
-  projectId: "user-administration-dff86",
-  storageBucket: "user-administration-dff86.appspot.com",
-  messagingSenderId: "55995338620",
-  appId: "1:55995338620:web:7d128d57d0b61ef185def9",
+    apiKey: "AIzaSyCe_GxNOKPP4dUp6sSa1Yy-fyFCjryj7KM",
+    authDomain: "user-administration-dff86.firebaseapp.com",
+    projectId: "user-administration-dff86",
+    storageBucket: "user-administration-dff86.firebasestorage.app",
+    messagingSenderId: "55995338620",
+    appId: "1:55995338620:web:7d128d57d0b61ef185def9",
+    measurementId: "G-WTCLCY1RB6"
 };
 
 const app = initializeApp(firebaseConfig);
